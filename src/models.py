9fe5f4d706cbd979d8ff10e944681c89c55681ea
Nico <nicosis@gmail.com>
2023-05-04 05:34:26 +0000

    work done

--- src/models.py
+++ src/models.py
@@ -5,46 +5,55 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-now = datetime.now()
+# now = datetime.now()
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
     email = Column(String(200), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
     password = Column(String(200), nullable=False)
-    current_time = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user_name = Column(String(200), nullable=False)
+    current_time = Column(DateTime, default=datetime.now,
+                          onupdate=datetime.now)
 
-    """ current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time) """
- 
- 
+    # current_time = now.strftime("%H:%M:%S")
+    # print("Current Time =", current_time)
+
+
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     climate = Column(String(80))
     description = Column(String(250))
-    name = Column(String(200), nullable=False)
-    rotation_period = Column(Integer, nullable=False)
-    url = Column(String(200), unique=True, nullable=False)
+    diameter = Column(Integer)
+    name = Column(String(80))
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    terrain = Column(Integer)
+    url = Column(String(200), unique=True)
+
 
 class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
     gender = Column(String(60))
-    url = Column(String(200), unique=True, nullable=False)
- 
+    mass = Column(Integer)
+    name = Column(String(60))
+    url = Column(String(200), unique=True)
+
+
 class Favorites(Base):
     __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    planet_id = Column(Integer, ForeignKey('planets.id') )
+    planet_id = Column(Integer, ForeignKey('planets.id'))
     characters_id = Column(Integer, ForeignKey('characters.id'))
-
 
 
 def to_dict(self):
